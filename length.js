0a65595c48a8c1c1a98883c3f458bb7a862a6090
Your Name <you@example.com>
2024-10-15 15:52:36 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,50 @@
+//check length
+
+const numbers =[1,5,10,20,28,32,39,25,46,59,];
+// console.log(numbers.length);
+
+//check length
+
+//check element
+//console.log(numbers[5]);
+// const x = [5];
+//check element
+
+//indexof
+//console.log(numbers.indexof(20));
+//indexof
+
+// //push
+// numbers.push(59)
+//console.log(numbers);
+//push
+
+//push
+const brands =["tanni","sanjida","jinia","sadia","mim",];
+//brands.push("Hermes")
+// console.log(brands);
+//push
+
+//pop
+//brands.pop("Hermes")
+// console.log(brands);
+//pop
+
+//unshift
+const brand =["tanni","sanjida","jinia","sadia","mim",];
+brands.unshift("Hermes");
+console.log(brand);
+//unshift
+
+//shift
+// const brand = ["tanni","sanjida","jinia","sadia","mim",];
+// brand.shift(Hermes);
+//console.log(brand);
+//shift
+
+
+
+const team = ["tanni","mim","sanjida","jinia",]
+//const x= team,includes("mim")
+// console.log(x)
+console.log(Array.isArray(team));
